b760fd1ff250970e13ead581c06a67c6750ce2a0
Ruslanos0 <Ruslanos0@yandex.ru>
2026-04-18 15:06:56 +0300

    goodsGroup routes added

---
+++ app/routers/goodsgroup.routes.js
@@ -0,0 +1,26 @@
+module.exports = app => {
+    
+    const goodsGroup = require("../controllers/goodsdroup.controller.js");
+
+    var router = require("express").Router();
+
+    // Create a new GoodsGroup
+    router.post("/", goodsGroup.create);
+
+    // Retrieve all GoodsGroup
+    router.get("/", goodsGroup.findAll);
+
+    // Retrieve a single GoodsGroup with id
+    router.get("/:id", goodsGroup.findOne);
+
+    // Update a GoodsGroup with id
+    router.put("/:id", goodsGroup.update);
+
+    // Delete a GoodsGroup with id
+    router.delete("/:id", goodsGroup.delete);
+
+    // Delete all GoodsGroups
+    router.delete("/", goodsGroup.deleteAll);
+
+    app.use('/api/goodsgroups', router);
+};
